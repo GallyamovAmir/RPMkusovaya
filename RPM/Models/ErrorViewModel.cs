3a37a7b516acc0d3f836110e43a82f71f2e70f2a
GallyamovAmir <amirgallyamov9967@gmail.com>
2023-03-23 21:54:05 +0300

    Добавьте файлы проекта.

---
+++ RPM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RPM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
